ee3d1628cd8db339a5345cf6f81a654357f2c551
Francesco Poinelli <francesco.poinelli@studenti.unitn.it>
2023-10-18 16:55:53 +0200

    refactor: refactoring

--- src/components/CustomDropDownMenu/CustomDropDownMenu.tsx
+++ src/components/CustomDropDownMenu/CustomDropDownMenu.tsx
@@ -1,12 +1,12 @@
 import {
-    Box,
-    Button,
-    //Flex,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuList,
-    Text,
+  Box,
+  Button,
+  //Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
 } from '@chakra-ui/react';
 
 import { ChevronDownIcon } from '@chakra-ui/icons';
@@ -19,93 +19,90 @@ import { useHasHydrated } from '../../utils/utils';
     useCollectionsContext
 } from '../CollectionsContext/CollectionsContext';*/
 
-export type onDataType =
-    | number
-    | string
+export type onDataType = number | string;
 type ArrayProps = {
-    name: string;
-    // oers: OerInCollectionProps[];
-    // conceptsSelected: OerConceptInfo[];
-    // color?: string;
-    // date?: Date;
+  name: string;
+  // oers: OerInCollectionProps[];
+  // conceptsSelected: OerConceptInfo[];
+  // color?: string;
+  // date?: Date;
 };
 type CollectionMenuProps = {
-    initialTitle: string; // Titolo iniziale del menu
-    data: ArrayProps[]; // Array di dati da scorrere nel menu
-    options?: string[] | undefined;
-    onData?: (data: string[] | number[]) => void;
+  initialTitle: string; // Titolo iniziale del menu
+  data: ArrayProps[]; // Array di dati da scorrere nel menu
+  options?: string[] | undefined;
+  onData?: (data: string[] | number[]) => void;
 };
 
 export default function CustomDropDownMenu({
-    initialTitle,
-    data, // Usa il prop data per popolare il menu
-    options,
-    onData,
+  initialTitle,
+  data, // Usa il prop data per popolare il menu
+  options,
+  onData,
 }: CollectionMenuProps) {
-    //const [selectedItem, setSelectedItem] = useState<string | null>(null);
-    const [menuTitle, setMenuTitle] = useState(initialTitle);
+  //const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [menuTitle, setMenuTitle] = useState(initialTitle);
 
-    const [selectedOptions] = useState<string[]>([]);
-    const [isOpen, setIsOpen] = useState(false); // for the open Menu
-    //const { collections } = useCollectionsContext();
-    const hydrated = useHasHydrated();
+  const [selectedOptions] = useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState(false); // for the open Menu
+  //const { collections } = useCollectionsContext();
+  const hydrated = useHasHydrated();
 
-    const handleData = () => {
-        if (onData) {
-            onData(selectedOptions);
-        }
-    };
-    const handleMenuItemClick = (item: ArrayProps) => {
-        //setSelectedItem(item.name);
-        setMenuTitle(item.name);
-        handleToggleMenu(); // Chiudi il menu dopo la selezione, se necessario
-    };
+  const handleData = () => {
+    if (onData) {
+      onData(selectedOptions);
+    }
+  };
+  const handleMenuItemClick = (item: ArrayProps) => {
+    //setSelectedItem(item.name);
+    setMenuTitle(item.name);
+    handleToggleMenu(); // Chiudi il menu dopo la selezione, se necessario
+  };
 
-    const handleToggleMenu = () => {
-        setIsOpen(!isOpen); // invert open menu state
-    };
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen); // invert open menu state
+  };
 
-    useEffect(() => {
-        handleData();
-    }, [selectedOptions]);
-    return (
-        <>
-            <Box flex="1">
-                <Menu
-                    isOpen={isOpen}
-                    onOpen={handleToggleMenu}
-                    onClose={handleToggleMenu}
-                >
-                    <MenuButton
-                        as={Button}
-                        rightIcon={<ChevronDownIcon />}
-                        //w={buttonWidth}
-                        w="100%"
-                    >
-                        {/* Could also use <Text align="left" overflow="hidden" whiteSpace="nowrap"> */}
-                        <Text align="left" noOfLines={1}>
-                            {selectedOptions.includes('All') &&
-                                (options?.length === selectedOptions.length)
-                                ? 'All'
-                                : selectedOptions.length > 0
-                                    ? selectedOptions.join(', ')
-                                    : menuTitle // Utilizza il valore memorizzato in menuTitle
-                            }
-                        </Text>
-                    </MenuButton>
+  useEffect(() => {
+    handleData();
+  }, [selectedOptions]);
+  return (
+    <>
+      <Box flex="1">
+        <Menu
+          isOpen={isOpen}
+          onOpen={handleToggleMenu}
+          onClose={handleToggleMenu}
+        >
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            //w={buttonWidth}
+            w="100%"
+          >
+            {/* Could also use <Text align="left" overflow="hidden" whiteSpace="nowrap"> */}
+            <Text align="left" noOfLines={1}>
+              {
+                selectedOptions.includes('All') &&
+                options?.length === selectedOptions.length
+                  ? 'All'
+                  : selectedOptions.length > 0
+                  ? selectedOptions.join(', ')
+                  : menuTitle // Utilizza il valore memorizzato in menuTitle
+              }
+            </Text>
+          </MenuButton>
 
-
-                    <MenuList>
-                        {hydrated && data?.map((item: ArrayProps, index: number) => (
-                            <MenuItem key={index} onClick={() => handleMenuItemClick(item)}>
-                                <Text>
-                                    {item.name}
-                                </Text>
-                            </MenuItem>
-                        ))}
-                    </MenuList>
-                </Menu>
-            </Box>
-        </>
-    );
+          <MenuList>
+            {hydrated &&
+              data?.map((item: ArrayProps, index: number) => (
+                <MenuItem key={index} onClick={() => handleMenuItemClick(item)}>
+                  <Text>{item.name}</Text>
+                </MenuItem>
+              ))}
+          </MenuList>
+        </Menu>
+      </Box>
+    </>
+  );
 }
--- src/pages/plan/index.tsx
+++ src/pages/plan/index.tsx
@@ -1,47 +1,41 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  Text,
-} from '@chakra-ui/react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import {
-  useCollectionsContext
-} from '../../components/CollectionsContext/CollectionsContext';
+import { useCollectionsContext } from '../../components/CollectionsContext/CollectionsContext';
 import CustomDropDownMenu from '../../components/CustomDropDownMenu/CustomDropDownMenu';
 import CustomSearchBar from '../../components/CustomSearchBar/CustomSearchBar';
 //import CheckboxMenu from '../../components/CheckboxMenu/CheckbomMenu';
 import Navbar from '../../components/NavBars/NavBarEncore';
 import SideBar from '../../components/SideBar/SideBar';
 //import SearchBar from '../../components/SearchBar/SearchBarEncore';
-import {
-  OerSkillInfo,
-} from '../../types/encoreElements';
-
-
+import { OerSkillInfo } from '../../types/encoreElements';
 
 const Home = (/*props: DiscoverPageProps*/) => {
   const { collections } = useCollectionsContext();
   const router = useRouter(); // router è un hook di next.js che fornisce l'oggetto della pagina corrente
   const { user } = useUser();
-  const [selectedCollection, setSelectedCollection] = useState<boolean | null>(null);
+  const [selectedCollection, setSelectedCollection] = useState<boolean | null>(
+    null
+  );
   const bloomLevels = [
-    { name: "Remember" },
-    { name: "Understand" },
-    { name: "Apply" },
-    { name: "Create" }
+    { name: 'Remember' },
+    { name: 'Understand' },
+    { name: 'Apply' },
+    { name: 'Create' },
   ];
 
   const [searchValue, setSearchValue] = useState<string[]>([]);
   const [selectedSkillIds, setSelectedSkillIds] = useState<number[]>([]);
   const [suggestions, setSuggestions] = useState<OerSkillInfo[]>([]);
 
-  useEffect(() => {setSuggestions}, []);
-  useEffect(() => {console.log("cambiato qualcosa")}, [selectedCollection]);
-  
+  useEffect(() => {
+    setSuggestions;
+  }, []);
+  useEffect(() => {
+    console.log('cambiato qualcosa');
+  }, [selectedCollection]);
 
   const handleCollectionSelection = () => {
     // Aggiorna lo stato per mostrare il testo quando viene selezionata una collection
@@ -57,7 +51,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
         <Flex
           w="100%"
           justifyContent="left"
-        //justify="space-between"
+          //justify="space-between"
         >
           <Heading>Learning path design</Heading>
         </Flex>
@@ -67,11 +61,13 @@ const Home = (/*props: DiscoverPageProps*/) => {
           w="100%"
           justifyContent="left"
           //justify="space-between"
-          >
+        >
           <Text>
-            This section is designed to assist you in crafting a personalized learning journey to achieve specific learning objectives.
+            This section is designed to assist you in crafting a personalized
+            learning journey to achieve specific learning objectives.
             <br />
-            It does so by suggesting potential educational activities and learning resources based on the desired learning objectives.
+            It does so by suggesting potential educational activities and
+            learning resources based on the desired learning objectives.
           </Text>
         </Flex>
 
@@ -80,7 +76,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
             Select here the collection that contain potential relevant resources
           </Text>
           <CustomDropDownMenu
-            initialTitle='Select Collection'
+            initialTitle="Select Collection"
             data={collections}
             onData={handleCollectionSelection}
           />
@@ -92,9 +88,12 @@ const Home = (/*props: DiscoverPageProps*/) => {
           w="100%"
           //justifyContent="left"
           //justify="space-between"
-          >
+        >
           <Text>
-            To define a learning path effectively, it is crucial to choose the desired level within the Bloom taxonomy and provide  indications of the skills, concepts, and contextual information that need to be achieved
+            To define a learning path effectively, it is crucial to choose the
+            desired level within the Bloom taxonomy and provide indications of
+            the skills, concepts, and contextual information that need to be
+            achieved
           </Text>
         </Flex>
 
@@ -114,10 +113,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
         {/*Component flex*/}
         <Flex w="100%">
           <Box paddingRight="3%" w="27%">
-            <CustomDropDownMenu
-              initialTitle='Bloom Level'
-              data={bloomLevels}
-            />
+            <CustomDropDownMenu initialTitle="Bloom Level" data={bloomLevels} />
           </Box>
 
           <Box paddingRight="3%" w="27%">
@@ -143,25 +139,26 @@ const Home = (/*props: DiscoverPageProps*/) => {
         {/*bottom box flex*/}
         <Flex w="100%">
           <Text fontSize="sm" paddingRight="3%" w="27%">
-            This level indicates the cognitive complexity or depth of understanding associated with a particular learning objective
+            This level indicates the cognitive complexity or depth of
+            understanding associated with a particular learning objective
           </Text>
           <Text fontSize="sm" paddingRight="3%" w="27%">
-            The selection of skills and concepts here is informed by the collection of Open Educational Resources (OERs).
+            The selection of skills and concepts here is informed by the
+            collection of Open Educational Resources (OERs).
           </Text>
           <Text fontSize="sm" paddingRight="3%" w="27%">
-            Here the contextual information that will assist in delineating the specific context of the educational activity
+            Here the contextual information that will assist in delineating the
+            specific context of the educational activity
           </Text>
         </Flex>
         <Flex paddingTop="1.5rem" w="100%">
           <Box w="50%">
             <Text fontSize="sm" paddingRight="3%">
-              Select the verbs related to your learning objective 
+              Select the verbs related to your learning objective
             </Text>
             {/*<CheckboxMenu />*/}
           </Box>
-          <Box w="50%"> 
-          </Box>
-          
+          <Box w="50%"></Box>
         </Flex>
       </Box>
     </Flex>
