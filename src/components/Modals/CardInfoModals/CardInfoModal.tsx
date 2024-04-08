685d384a824d605c1ba38ca61f2480b8f0d6b586
Edoardo Fiorentino <edo-fiore@hotmail.it>
2024-04-08 11:29:55 +0200

    refactor: refactor

--- src/components/Modals/CardInfoModals/CardInfoModal.tsx
+++ src/components/Modals/CardInfoModals/CardInfoModal.tsx
@@ -253,7 +253,7 @@ export default function CardInfoModal({
           setLastUpdate(oer?.retrieval_date || oer?.publication_date || '');
           setCoverage(
             oer.coverage?.map((audience: OerAudienceInfo) => audience.name) ||
-            []
+              []
           );
           setSource_roer(
             oer?.source_roer?.map((item: OerSourceRoerInfo) => item.name) || []
@@ -560,9 +560,9 @@ export default function CardInfoModal({
           collections={collections}
           addResource={addResource}
           addCollection={addCollection}
-        //times_used={times_used}
-        //setTimes_used={setTimes_used}
-        //getCount={getCount}
+          //times_used={times_used}
+          //setTimes_used={setTimes_used}
+          //getCount={getCount}
         />
       )}
       <ExerciseInfoModal
