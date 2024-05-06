c1456b8b49f014e598f0746f0f9758271eefd544
Edoardo Fiorentino <edo-fiore@hotmail.it>
2024-05-06 02:44:42 +0200

    refactor: to push

--- src/pages/design/LearningObjective.tsx
+++ src/pages/design/LearningObjective.tsx
@@ -252,7 +252,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
           <Flex
             w="100%"
             justifyContent="left"
-          //justify="space-between"
+            //justify="space-between"
           >
             <Heading>Learning path design</Heading>
           </Flex>
@@ -261,7 +261,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
             paddingTop="1.5rem"
             w="100%"
             justifyContent="left"
-          //justify="space-between"
+            //justify="space-between"
           >
             <Box
               //  w={isSmallerScreen ? '95%' : '90%'}
@@ -339,8 +339,8 @@ const Home = (/*props: DiscoverPageProps*/) => {
                 setIsNextButtonClicked={setIsNextButtonClicked}
                 isHighligted={isNextButtonClicked}
                 isSmallerScreen={isSmallerScreen}
-              // apiKey={apiKey}
-              // handleApiKey={handleApiKey}
+                // apiKey={apiKey}
+                // handleApiKey={handleApiKey}
               />
             </Flex>
           )}
