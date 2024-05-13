9aea302078718b6419b1e9b6a237923ff67aefa0
Edoardo Fiorentino <edo-fiore@hotmail.it>
2024-05-13 10:17:06 +0200

    refactor: refactor to push

--- src/components/Views/PathDesignViews/PathDesignGenLessonPlan/index.tsx
+++ src/components/Views/PathDesignViews/PathDesignGenLessonPlan/index.tsx
@@ -12,7 +12,6 @@ import GenerateLessonPlanButton from '../../../Buttons/ButtonsDesignPage/Generat
 import RowBoxGenLessonPlan from './RowBoxGenLessonPlan';
 
 export default function PathDesignGenLessonPlan() {
-
   const { chosenTargetLevel } = useCreateOERsContext();
 
   const [numberOfLearningActivities, setNumberOfLearningActivities] =
@@ -94,8 +93,8 @@ export default function PathDesignGenLessonPlan() {
         newIndexBloomActivity
       ]?.includes(newIndexActivity)
         ? learningActivitiesIndex[newIndexBloomActivity].filter(
-          (index) => index !== newIndexActivity
-        ) // If YES, remove the new index activity from the array
+            (index) => index !== newIndexActivity
+          ) // If YES, remove the new index activity from the array
         : [...learningActivitiesIndex[newIndexBloomActivity], newIndexActivity]; // If NO, add the new index activity to the array
 
       // Update the learning activities index array with the new index activity
