df9f5ff099e89eb4f0e4bc3bd57fa84065b5db7b
edofiore <edo-fiore@hotmail.it>
2024-05-28 22:01:16 +0200

    refactor: refactor to push

--- src/components/SegmentedButtonGroup/SegmentedButtonGroup.tsx
+++ src/components/SegmentedButtonGroup/SegmentedButtonGroup.tsx
@@ -156,11 +156,11 @@ export default function SegmentedButtonGroup({
                   isHighlighted={highlightedOptions.includes('YourExperience')}
                   isSmallerScreen={isSmallerScreen}
                 />
-                {isNextButtonClicked && selectedEducatorExperience === null &&
+                {isNextButtonClicked && selectedEducatorExperience === null && (
                   <Text color="error_label" fontSize="sm">
                     Select an educator experience!
                   </Text>
-                }
+                )}
               </Box>
             </Box>
             <Box w="50%">
@@ -174,11 +174,11 @@ export default function SegmentedButtonGroup({
                   isHighlighted={highlightedOptions.includes('Context')}
                   isSmallerScreen={isSmallerScreen}
                 />
-                {isNextButtonClicked && selectedContext === null &&
+                {isNextButtonClicked && selectedContext === null && (
                   <Text color="error_label" fontSize="sm">
                     Select an educational context!
                   </Text>
-                }
+                )}
               </Box>
             </Box>
           </Flex>
@@ -195,11 +195,11 @@ export default function SegmentedButtonGroup({
                   isHighlighted={highlightedOptions.includes('GroupDimension')}
                   isSmallerScreen={isSmallerScreen}
                 />
-                {isNextButtonClicked && selectedGroupDimension === null &&
+                {isNextButtonClicked && selectedGroupDimension === null && (
                   <Text color="error_label" fontSize="sm">
                     Select a group dimension!
                   </Text>
-                }
+                )}
               </Box>
             </Box>
             <Box w="50%">
@@ -215,11 +215,11 @@ export default function SegmentedButtonGroup({
                   )}
                   isSmallerScreen={isSmallerScreen}
                 />
-                {isNextButtonClicked && selectedLearnerExperience === null &&
+                {isNextButtonClicked && selectedLearnerExperience === null && (
                   <Text color="error_label" fontSize="sm">
                     {`Select a learner\'s experience!`}
                   </Text>
-                }
+                )}
               </Box>
             </Box>
           </Flex>
