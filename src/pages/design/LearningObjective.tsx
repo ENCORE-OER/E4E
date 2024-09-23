bde2744ad7ad88aa3e26bd4c8839687a3631614f
Edoardo Fiorentino <edo-fiore@hotmail.it>
2024-09-23 19:13:48 +0200

    feat: update APIs configuration

--- src/pages/design/LearningObjective.tsx
+++ src/pages/design/LearningObjective.tsx
@@ -207,7 +207,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
         handleIdLearningScenario('');
         handleEditLessonPlanClick(true);
 
-        console.log("SONO NELL'HANDLE GENERATION FUNCTION");
+        // console.log("SONO NELL'HANDLE GENERATION FUNCTION");
         // Generate Lesson Plan
         const isPossibleToContinue = await handleGenerationFunction();
 
@@ -358,7 +358,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
           <Flex
             w="100%"
             justifyContent="left"
-            //justify="space-between"
+          //justify="space-between"
           >
             <Heading>Learning path design</Heading>
           </Flex>
@@ -367,7 +367,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
             paddingTop="1.5rem"
             w="100%"
             justifyContent="left"
-            //justify="space-between"
+          //justify="space-between"
           >
             <Box
               //  w={isSmallerScreen ? '95%' : '90%'}
@@ -455,7 +455,7 @@ const Home = (/*props: DiscoverPageProps*/) => {
             handleNextClick={
               lessonActivities.length > 0 ? handleNextClick : undefined
             }
-            // handlePrevButtonClick={handlePrevButtonClick}
+          // handlePrevButtonClick={handlePrevButtonClick}
           />
         </Box>
       </Box>
